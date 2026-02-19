cb3aafd99c3e7c34c71322ca115fc494014857b2
WayneCarlW <waynecarlgeno@gmail.com>
2026-02-20 00:27:54 +0300

    Added two logic building problems: Swapping and close and divisible

---
+++ Logic-Building/close-n-divisible-m.py
@@ -0,0 +1,3 @@
+!#usr/bin/env python3
+
+def close_n_divisible_m()
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/kalpix/Desktop/DSA/refresher/venv
+set -gx VIRTUAL_ENV /home/kalpix/Desktop/DSA/DSA-REFRESHER/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
